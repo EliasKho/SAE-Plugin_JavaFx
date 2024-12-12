3e819aca615017650c528cf44bee78a474ffd04a
EliasKho <elias.khodjaoui@free.fr>
2024-12-12 12:33:23 +0100

    modele + interface basique

--- src/main/java/projet/Modele.java
+++ src/main/java/projet/Modele.java
@@ -1,15 +1,34 @@
 package projet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Modele {
 
     private String path;
+    private List<Observateur> observateurs;
 
     Modele(String path) {
         this.path = path;
+        this.observateurs = new ArrayList<Observateur>();
     }
 
     public String getPath() {
         return path;
+    }
+
+    public void notifierObservateurs(){
+        for(Observateur observateur : observateurs){
+            observateur.actualiser();
+        }
+    }
+
+    public void enregistrerObservateur(Observateur observateur){
+        observateurs.add(observateur);
+    }
+
+    public void supprimerObservateur(Observateur observateur){
+        observateurs.remove(observateur);
     }
 
 }
