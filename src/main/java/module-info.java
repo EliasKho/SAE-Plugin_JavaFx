8bfe8159f6ac564f88254e7df79d3ba7959c0f4d
Léo Fontaine <leo.fontaine4@orange.fr>
2024-12-12 11:50:32 +0100

    modification certains paramètres javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,6 @@
 module com.example.saeplugin_javafx {
     requires javafx.controls;
-    requires javafx.fxml;
     requires javafx.graphics;
-
-
-    opens com.example.saeplugin_javafx to javafx.fxml;
 
     exports projet;
 }
--- src/main/java/projet/main.java
+++ src/main/java/projet/main.java
@@ -24,10 +24,10 @@ public class main extends Application {
     @Override
     public void start(Stage stage) {
 
-        String [] ingredients={"Fromage","Champignons","Chorizo","Oeuf","Oignons","Olives noires","Olives vertes","Roquette"};
+        String[] ingredients = {"Fromage", "Champignons", "Chorizo", "Oeuf", "Oignons", "Olives noires", "Olives vertes", "Roquette"};
         // String[] fidelite = {"Nouveau client", "Cliente avec carte", "Client adhérent"};
         Button[] bIngr;
-        BorderPane bp= new BorderPane();
+        BorderPane bp = new BorderPane();
         bp.setPadding(new Insets(10));
 
 
@@ -37,33 +37,33 @@ public class main extends Application {
         pnord.setPadding(new Insets(10));
         pnord.setAlignment(Pos.CENTER);
 
-        ComboBox<String> choixFidelite = new ComboBox<String>( );
-        choixFidelite .getItems().add("Nouveau client");
-        choixFidelite .getItems().add("Client adhérent");
-        choixFidelite .getItems().add("Cliente avec carte");
+        ComboBox<String> choixFidelite = new ComboBox<String>();
+        choixFidelite.getItems().add("Nouveau client");
+        choixFidelite.getItems().add("Client adhérent");
+        choixFidelite.getItems().add("Cliente avec carte");
         choixFidelite.setValue("Nouveau client");
 
-        Button  addPizzaCreme= new Button(" Ajouter une pizza fond creme ");
-        Button addPizzaTomate= new Button(" Ajouter une pizza fond tomate ");
+        Button addPizzaCreme = new Button(" Ajouter une pizza fond creme ");
+        Button addPizzaTomate = new Button(" Ajouter une pizza fond tomate ");
         pnord.getChildren().addAll(choixFidelite, addPizzaCreme, addPizzaTomate);
         bp.setTop(pnord); //place pnord en haut de l'IG
 
 
         // Panneau au centre de l'IG contenant la vision du choix des pizzas
         // ainsi que les boutons pour ajouter des ingredients
-        BorderPane pcentral= new BorderPane();
+        BorderPane pcentral = new BorderPane();
         pcentral.setMaxHeight(300);
 
         //--> Le panneau avec la vision des images des pizzas
         //--> Le panneau contenant les boutons des ingredrients
-        GridPane pingr= new GridPane();
+        GridPane pingr = new GridPane();
         pingr.setAlignment(Pos.CENTER);
         pingr.setHgap(10);
-        bIngr= new Button[8];
-        for(int i=0;i<ingredients.length;i++){
-            bIngr[i]=new Button(ingredients[i]);
-            bIngr[i].setMinSize(100,40);
-            pingr.add(bIngr[i],i,0);
+        bIngr = new Button[8];
+        for (int i = 0; i < ingredients.length; i++) {
+            bIngr[i] = new Button(ingredients[i]);
+            bIngr[i].setMinSize(100, 40);
+            pingr.add(bIngr[i], i, 0);
         }
         // pingr.setPreferredSize(new Dimension(935,50));
         pcentral.setBottom(pingr);
@@ -71,15 +71,15 @@ public class main extends Application {
 
         // Panneau au sud de l'IG dans lequel se trouve l'affichage
         // du contenu de la commande et son prix
-        BorderPane psud= new BorderPane();
+        BorderPane psud = new BorderPane();
         psud.setMinHeight(300);
 
-        Label commtxt= new Label("Aucune commande en cours");
+        Label commtxt = new Label("Aucune commande en cours");
         commtxt.setFont(new Font("Times", 14));
         //  commtxt.setPreferredSize(new Dimension(935,200));
         psud.setCenter(commtxt);
         //Mettre un panneau VBox
-        VBox vb= new VBox();
+        VBox vb = new VBox();
         vb.setAlignment(Pos.CENTER);
         Label txtBas = new Label("Pas de commande en cours");
         txtBas.setFont(new Font("Times", 16));
@@ -89,7 +89,7 @@ public class main extends Application {
         bp.setBottom(psud);
 
 
-        Scene scene = new Scene(bp,935,670);
+        Scene scene = new Scene(bp, 935, 670);
         stage.setTitle("Commande de pizzas");
         stage.setScene(scene);
         stage.show();
